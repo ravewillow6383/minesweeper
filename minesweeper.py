4133449b0b8ea24edb326afd26517813b3c67e52
Raven Robertson <ravewillow6383@gmail.com>
2019-09-23 13:14:14 -0700

    building up Tile class

--- minesweeper.py
+++ minesweeper.py
@@ -1,7 +1,21 @@
+from random import randint
+
+# global constants
+width = 40
+NUM_BOMBS = 10
+
+class Tile:
+    def __init__(self):
+        self.bomb = False
+        
+grid = [[Tile() for n in range(9)] for n in range (9)]
+
+for n in range(NUM_BOMB):
+
+
 def setup():
-    pass
+    size(800,600)
 
 def draw():
     pass
 
-size(800,600)
