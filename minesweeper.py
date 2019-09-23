137c6955ac24932feeeb169ca26fa237d4ac0378
Raven Robertson <ravewillow6383@gmail.com>
2019-09-23 13:22:11 -0700

    adding grid to code

--- minesweeper.py
+++ minesweeper.py
@@ -10,12 +10,30 @@ class Tile:
         
 grid = [[Tile() for n in range(9)] for n in range (9)]
 
+
+#Place 10 bombs at random tiles
 for n in range(NUM_BOMB):
 
+    while True:
+        x = randint(0, 8)
+        y = randint(0, 8)
+        if grid[y][x].bomb == False:
+            grid[y][x].bomb = True
+            break
 
 def setup():
-    size(800,600)
+    size(360,360)
 
 def draw():
-    pass
+    y = 0
+    for row in grid:
+        x = 0
+        for tile in row:
+            if tile.bomb == True:
+                fill(255,0,0)
+            else:
+                fill(255)
+            rect(x, y, width, width)
+            x += width
+        y+=width
 
